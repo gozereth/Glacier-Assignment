0bd3f591c290bb1d1e071d5a9b5793b5bbab15dc
gozereth <nlemekechigozie@gmail.com>
2024-08-03 03:17:00 +0100

    scripts and test results

---
+++ scripts/new .py
@@ -0,0 +1,8 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: /Users/mac/Desktop/data-glacier/VC/scripts/add.py ==========
+Please add your name: Chigozie Nlemeke
+Please add your favourite sports name: Football
+Please add your name: Chigozie Nlemeke
+Please add your favourite sports name: Football
